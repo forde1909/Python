d09540a351e76099165fdac9b562828185f66ed2
JForde <73252229+forde1909@users.noreply.github.com>
2022-12-26 19:59:27 +0000

    Delete 6a directory

--- 6a/mylib/__init__.py
+++
@@ -1,4 +0,0 @@
-
-
-copyright = "© JOR 2022"
-copyright = "© JF 2022"
--- 6a/square_cube.py
+++
@@ -1,9 +0,0 @@
-
-# To find Square and Cube of a number
-def square (num):
-return num*num #Square of a number
-def cube (num):
-return num*num*num #Cube of a number
-number = int(input("Enter an integer number: "))
-print("Square of ",number," is ", square(number))
-print("Cube of ",number," is ", cube(number))
